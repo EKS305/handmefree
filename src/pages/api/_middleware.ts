687aa8a108906174ae1b811301ec17b9b7fb16c5
EKS305 <jihan10000@proton.me>
2026-02-04 23:01:42 +0100

    Remove Astro API routes to allow Pages Functions handling /api

--- src/pages/api/_middleware.ts
+++
@@ -1,7 +0,0 @@
-﻿import type { MiddlewareHandler } from "astro";
-
-export const onRequest: MiddlewareHandler = async (context, next) => {
-  // Disable Astro CSRF ONLY for API routes
-  context.locals.disableAstroCSRF = true;
-  return next();
-};
